ac90f503c0c673a03d3c7e34af9fbe6b064230f8
SeifSultan03 <ssulta7@lsu.edu>
2022-08-13 22:57:14 -0500

    serve favicon

--- Server.js
+++ Server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+var favicon = require('serve-favicon');
 const app = express();
 const server = require('http').createServer(app);
 const io = require('socket.io')(server, {
